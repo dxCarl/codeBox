5bd4660ad56e59d73b4c48f2ccdec15354b91f9d
dxCarl <31230094+dxCarl@users.noreply.github.com>
2018-07-02 17:05:20 +0800

    jdbctemplate 方式获取新插入数据的自增id 值
    HOW TO GET AUTO-GENERATED KEY WITH JDBCTEMPLATE

---
+++ java/GeneratedKeyDemo.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+public class GeneratedKeyDemo {
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  public long addNew(final String name) {
+    final PreparedStatementCreator psc = new PreparedStatementCreator() {
+      @Override
+      public PreparedStatement createPreparedStatement(final Connection connection) throws SQLException {
+        final PreparedStatement ps = connection.prepareStatement("INSERT INTO `names` (`name`) VALUES (?)",
+            Statement.RETURN_GENERATED_KEYS);
+        ps.setString(1, name);
+        return ps;
+      }
+    };
+
+    // The newly generated key will be saved in this object
+    final KeyHolder holder = new GeneratedKeyHolder();
+
+    jdbcTemplate.update(psc, holder);
+
+    final long newNameId = holder.getKey().longValue();
+    return newNameId;
+  }
+}
